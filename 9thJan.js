b86ffcda39fb1d340e29068bff2e82fc59103515
celya <yjc3345@naver.com>
2023-01-16 11:27:18 +0900

    16thJan

--- 9thJan.js
+++ 9thJan.js
@@ -1,12 +1,15 @@
-const logIn=document.getElementById("login");
-const loginModal=document.getElementsByClassName("modal");
-const noModal=document.getElementById("x");
-
-logIn.onclick = function() {
-    loginModal.style.display = "block";
+document.getElementById("login").onclick = function() {
+    document.getElementsByClassName("modal")[0].style.display = "inline";
 }
-noModal.onclick = function() {
-    loginModal.style.display = "none";
+document.getElementById("x").onclick = function() {
+    document.getElementsByClassName("modal")[0].style.display = "none";
 }
 
 
+
+// document.getElementById("login").addEventListener('click', function() {
+//     document.getElementsByClassName("modal").style.display = "block";
+// });
+// document.getElementById("x").addEventListener('click', function() {
+//     document.getElementsByClassName("modal").style.display = "none";
+// });
