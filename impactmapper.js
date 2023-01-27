7bc9e89bb9efa319eed508ae0447c5478241e615
yjc3345 <yjc3345@naver.com>
2023-01-27 17:49:22 +0900

    27thJan

---
+++ impactmapper.js
@@ -0,0 +1,17 @@
+const request = document.getElementById("request");
+const logIn = document.getElementById("LogIn");
+const x = document.getElementsByClassName("X");
+
+request.onclick= requested;
+logIn.onclick=onClicked;
+x.onclick=remove;
+
+function requested(){
+   alert('오 이런');
+}
+
+function remove(){
+
+}
+
+
