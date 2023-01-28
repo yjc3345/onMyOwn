f5a1e7697ea2db80e8f1cf0a48e19cd8dceed108
yjc3345 <yjc3345@naver.com>
2023-01-28 10:07:11 +0900

    28thJan

--- impactmapper.js
+++ impactmapper.js
@@ -1,17 +1,36 @@
 const request = document.getElementById("request");
 const logIn = document.getElementById("LogIn");
-const x = document.getElementsByClassName("X");
+const x = document.querySelector(".X");
+const modal = document.querySelector(".modalBackground");
+request.onclick= requestOnClick;
+logIn.onclick= loginOnClick;
+x.onclick= xOnClick;
 
-request.onclick= requested;
-logIn.onclick=onClicked;
-x.onclick=remove;
-
-function requested(){
-   alert('오 이런');
+function requestOnClick(){
+    alert('오 이런');
 }
 
-function remove(){
+function loginOnClick(){
+    display("block"); 
+}
 
+function xOnClick(){
+    display("none"); 
+}
+
+function display(yay){
+    modal.style.display = yay;
 }
 
 
+
+
+// const x = document.getElementsByClassName("X");
+// const modal = document.getElementsByClassName("modalBackground");
+
+// function display(yay){
+//     modal[0].style.display = yay;
+// }
+// x[0].onclick= xOnClick;
+
+// ElementsByClassName을 사용하면 배열처럼 가져오기 때문에 [0]을 넣어야한다.ㅠ
