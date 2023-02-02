f6cf7940f84cf41c6039c99bb78bc556b854cecb
yjc3345 <yjc3345@naver.com>
2023-02-02 18:20:49 +0900

    2ndFeb

--- impactmapper.js
+++ impactmapper.js
@@ -1,55 +1,52 @@
-const request = document.getElementById("request");
-const logIn = document.querySelector(".LogIn");
-const x = document.querySelectorAll(".X");
-const modal = document.querySelectorAll(".modalBackground");
-const enterLogin = document.querySelectorAll(".enterLogin");
-const enterSignin = document.querySelectorAll(".enterSignin");
+const request = document.getElementById('request');
+const logIn = document.querySelector('.LogIn');
+const x = document.querySelectorAll('.X');
+const modal = document.querySelectorAll('.modalBackground');
+const enterLogin = document.querySelectorAll('.enterLogin');
+const enterSignin = document.querySelectorAll('.enterSignin');
 
-function enterSigninOnClick(){
-    loginModalDisplay("none");
-    signinModalDisplay("block");
+function enterSigninOnClick() {
+	loginModalDisplay('none');
+	signinModalDisplay('block');
 }
 
-function enterOnClick(){
-    alert("없는 아이디 입니다.")
+function enterOnClick() {
+	alert('없는 아이디 입니다.');
 }
 
-function requestOnClick(){
-    alert('오 이런');
+function requestOnClick() {
+	alert('오 이런');
 }
 
-function loginOnClick(){
-    loginModalDisplay("block"); 
-    signinModalDisplay("none");
+function loginOnClick() {
+	loginModalDisplay('block');
+	signinModalDisplay('none');
 }
 
-function xOnClick(){
-    loginModalDisplay("none"); 
-    signinModalDisplay("none");
+function xOnClick() {
+	loginModalDisplay('none');
+	signinModalDisplay('none');
 }
 
-function loginModalDisplay(yay){
-    modal[0].style.display = yay;
+function loginModalDisplay(yay) {
+	modal[0].style.display = yay;
 }
-function signinModalDisplay(yay){
-    modal[1].style.display = yay;
-}
-
-function signedIn(){
-    alert("회원가입 실패")
+function signinModalDisplay(yay) {
+	modal[1].style.display = yay;
 }
 
-request.onclick= requestOnClick;
-logIn.onclick= loginOnClick;
-x[0].onclick= xOnClick;
-x[1].onclick= xOnClick;
-enterLogin[0].onclick= enterOnClick;
-enterSignin[0].onclick= enterSigninOnClick;
-enterSignin[1].onclick= signedIn;
-enterLogin[1].onclick=loginOnClick;
+function signedIn() {
+	alert('회원가입 실패');
+}
 
-
-
+request.onclick = requestOnClick;
+logIn.onclick = loginOnClick;
+x[0].onclick = xOnClick;
+x[1].onclick = xOnClick;
+enterLogin[0].onclick = enterOnClick;
+enterSignin[0].onclick = enterSigninOnClick;
+enterSignin[1].onclick = signedIn;
+enterLogin[1].onclick = loginOnClick;
 
 // const x = document.getElementsByClassName("X");
 // const modal = document.getElementsByClassName("modalBackground");
