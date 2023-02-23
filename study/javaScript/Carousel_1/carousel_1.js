664ad7a9206296d62d9057f638f9c218e5229474
yjc3345 <yjc3345@naver.com>
2023-02-23 10:52:15 +0900

    23rdFeb

--- study/javaScript/Carousel_1/carousel_1.js
+++ study/javaScript/Carousel_1/carousel_1.js
@@ -1,23 +1,23 @@
-// document.querySelector('.button1').addEventListener('click', function () {
-// 	document.querySelector('.container').style.transform = 'translateX(0vw)';
-// });
-// document.querySelector('.button2').addEventListener('click', function () {
-// 	document.querySelector('.container').style.transform = 'translateX(-80vw)';
-// });
-// document.querySelector('.button3').addEventListener('click', function () {
-// 	document.querySelector('.container').style.transform = 'translateX(-160vw)';
-// });
-// document.querySelector('.button4').addEventListener('click', function () {
-// 	document.querySelector('.container').style.transform = 'translateX(-240vw)';
-// });
-const button = document.querySelectorAll('.button');
-const container = document.querySelector('.container');
+document.querySelector('.button1').addEventListener('click', function () {
+	document.querySelector('.container').style.transform = 'translateX(0vw)';
+});
+document.querySelector('.button2').addEventListener('click', function () {
+	document.querySelector('.container').style.transform = 'translateX(-80vw)';
+});
+document.querySelector('.button3').addEventListener('click', function () {
+	document.querySelector('.container').style.transform = 'translateX(-160vw)';
+});
+document.querySelector('.button4').addEventListener('click', function () {
+	document.querySelector('.container').style.transform = 'translateX(-240vw)';
+});
+// const button = document.querySelectorAll('.button');
+// const container = document.querySelector('.container');
 
-for (i = 0; i < button.length; i++) {
-	button[i].addEventListener('click', transform);
-	console.log(i);
-}
-function transform() {
-	container.style.transform = `translateX(calc(-80 * ${i}vw))`;
-	console.log(i);
-}
+// for (i = 0; i < button.length; i++) {
+// 	button[i].addEventListener('click', transform);
+// 	console.log(i);
+// }
+// function transform() {
+// 	container.style.transform = `translateX(calc(-80 * ${i}vw))`;
+// 	console.log(i);
+// }
