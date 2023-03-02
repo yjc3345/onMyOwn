99f3b2053720a597426ebc66553f265f28d09774
yjc3345 <yjc3345@naver.com>
2023-03-02 15:14:21 +0900

    2ndMar

--- study/typingGame/index.js
+++ study/typingGame/index.js
@@ -1,1 +1,38 @@
 //사용변수
+let score = 0;
+let time = 9;
+let isPlaying = false;
+
+const wordInput = document.querySelector('.wordInput');
+const wordDisplay = document.querySelector('.wordDisplay');
+const scoreDisplay = document.querySelector('.score');
+const timeDisplay = document.querySelector('.time');
+const button = document.querySelector('button');
+
+wordInput.addEventListener('input', () => {
+	if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+		score++;
+		scoreDisplay.innerHTML = score;
+		wordInput.value = '';
+	}
+});
+
+buttonChange('게임시작');
+
+function run() {
+	timeInterval = setInterval(countDown, 1000);
+}
+
+function countDown() {
+	//삼항연산자
+	// (조건) ? 참일경우 : 거짓일경우
+	time > 0 ? time-- : (isPlaying = false);
+	timeDisplay.innerText = time;
+}
+
+function buttonChange(text) {
+	button.innerText = text;
+	text === '게임시작'
+		? button.classList.remove('loading')
+		: button.classList.add('loading');
+}
