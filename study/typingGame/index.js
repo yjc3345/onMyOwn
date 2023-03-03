a3f7fb0dce2f15a7d8fde923bc2deb138bff8ee0
yjc3345 <yjc3345@naver.com>
2023-03-03 15:13:19 +0900

    3rdMar

--- study/typingGame/index.js
+++ study/typingGame/index.js
@@ -1,32 +1,65 @@
 //사용변수
+const gameTime = 9;
 let score = 0;
-let time = 9;
+let time;
 let isPlaying = false;
-
+let words = [];
 const wordInput = document.querySelector('.wordInput');
 const wordDisplay = document.querySelector('.wordDisplay');
 const scoreDisplay = document.querySelector('.score');
 const timeDisplay = document.querySelector('.time');
-const button = document.querySelector('button');
+const button = document.querySelector('.button');
 
-wordInput.addEventListener('input', () => {
-	if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
-		score++;
-		scoreDisplay.innerHTML = score;
-		wordInput.value = '';
+init();
+
+function init() {
+	getWords();
+	wordInput.addEventListener('input', checkMatch);
+}
+
+function checkStatus() {
+	if (isPlaying && time === 0) {
+		buttonChange('게임시작');
+		clearInterval(checkInterval);
 	}
-});
+}
 
-buttonChange('게임시작');
+// 단어 불러오기
+function getWords() {
+	words = ['hello', 'apple', 'orange'];
+	buttonChange('게임시작');
+}
 
+//단어 일치 체크
+function checkMatch() {
+	if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+		wordInput.value = '';
+		if (!isPlaying) {
+			return;
+		}
+		score++;
+		scoreDisplay.innerText = score;
+		time = gameTime;
+	}
+}
+//게임 실행
 function run() {
+	isPlaying = true;
+	time = gameTime;
+	wordInput.focus();
+	scoreDisplay.innerText = 0;
 	timeInterval = setInterval(countDown, 1000);
+	checkInterval = setInterval(checkStatus, 50);
+	buttonChange('게임중');
 }
 
 function countDown() {
 	//삼항연산자
 	// (조건) ? 참일경우 : 거짓일경우
 	time > 0 ? time-- : (isPlaying = false);
+	if (!isPlaying) {
+		clearInterval(timeInterval);
+	}
 	timeDisplay.innerText = time;
 }
 
